6406023bfc168b5924d9ff201c6f76d02723c488
bluegalaxy6 <rumil_yavetil@hotmail.com>
2023-05-27 23:26:25 +0200

    Añadido Animator basico
    He añadido 2 imagenes (una dentro de los objetos padres de calle principal y otra dentro de calle chica).
    
    Tambien he añadido los sprites en su carpeta correspondiente y creado un script llamado "Animator" que solo sirve para hacer el getcomponent en los objetos que tengan animator.

---
+++ Assets/Animator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   
+}
