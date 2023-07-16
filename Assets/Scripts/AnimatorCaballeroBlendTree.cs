12e0bd59e47377f42e3a32df76320fd920671580
bluegalaxy6 <rumil_yavetil@hotmail.com>
2023-07-16 02:12:30 +0200

    Cinematica Inicial
    (Dentro del MainCamera)

--- Assets/Scripts/AnimatorCaballeroBlendTree.cs
+++ Assets/Scripts/AnimatorCaballeroBlendTree.cs
@@ -4,38 +4,35 @@ using UnityEngine;
 
 public class AnimatorCaballeroBlendTree : MonoBehaviour
 {
-
     [SerializeField] Click2D ReferenciaClick2D;
     Animator Animator;
-    
-
-
 
     void Start()
     {
-        Animator = GetComponent<Animator>();
-        
+        Animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = (ReferenciaClick2D.BlendTreeValue.x*5);
-        float y = (ReferenciaClick2D.BlendTreeValue.y*5);
+        TomaDeReferencias();
 
 
-        Animator.SetFloat("x", -x);
-        Animator.SetFloat("y", -y);
 
     }
 
-    void ActualizaPosicion()
+    void ActualizaPosicion() // esto se lanza a traves de cada frame de las animaciones
     {
-        Debug.Log("actualiza frame)");
-
         transform.position = ReferenciaClick2D.transform.position;
         transform.localScale = ReferenciaClick2D.transform.localScale;
-        
+    }
+
+    void TomaDeReferencias()
+    {
+        float x = (ReferenciaClick2D.BlendTreeValue.x * 5);
+        float y = (ReferenciaClick2D.BlendTreeValue.y * 5);
+        Animator.SetFloat("x", -x);
+        Animator.SetFloat("y", -y);
     }
 
 }
